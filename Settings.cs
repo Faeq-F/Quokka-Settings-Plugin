a74410ff7000ea9713ce07f44b1c47493405c9ea
Faeq <faeqfaisal@hotmail.co.uk>
2025-02-22 16:16:44 +0000

    :children_crossing: adjusted fuzzySearchThreshold for easier use

--- Settings.cs
+++ Settings.cs
@@ -40,10 +40,10 @@ public class PluginSettings {
   /// <summary>
   ///   The threshold for when to consider a settings page
   ///   name is similar enough to the query for it to be
-  ///   displayed (defaults to 5). Currently uses the
+  ///   displayed (defaults to 4). Currently uses the
   ///   Levenshtein distance; the larger the number, the
   ///   bigger the difference.
   /// </summary>
-  public int FuzzySearchThreshold { get; set; } = 5;
+  public int FuzzySearchThreshold { get; set; } = 4;
 
 }
